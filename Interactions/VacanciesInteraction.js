794f2aac9a46a27e5bc0aceacd307fe0e1bedc67
Mrksist <platon_fil08@mail.ru>
2024-10-31 02:12:48 +0300

    Changes V3.1

--- Interactions/VacanciesInteraction.js
+++ Interactions/VacanciesInteraction.js
@@ -1,5 +1,9 @@
-const goToVacancies = function() {
-
+const goToVacancies = function(bot,msg,localization,dbmaster) {
+  bot.answerCallbackQuery(msg.id).then(()=>{
+    bot.sendMessage(msg.message.chat.id, "Вот все твои вакансии:");
+  }).then(()=>{
+    bot.deleteMessage(msg.message.chat.id,msg.message.message_id);
+  })
 }
 
 module.exports = goToVacancies;
